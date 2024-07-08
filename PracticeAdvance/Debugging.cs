43b894c1ef54aa8d865498f559366d649c88873e
gillvineet <gillvineet243@gmail.com>
2024-07-08 13:15:34 +0530

    Revert "Update Debugging.cs"
    This reverts commit 804b6e16734dc29cbf683f197438022b7f7cc88a.

--- PracticeAdvance/Debugging.cs
+++ PracticeAdvance/Debugging.cs
@@ -20,7 +20,7 @@ namespace PracticeAdvance
             string name = "HI i am vineet gill";
             string[] arr = name.Split(' ');
             Console.WriteLine(c);
-            Console.WriteLine("heelo changes made");
+            Console.WriteLine("we are making changes i repo");
 
         }
     }
