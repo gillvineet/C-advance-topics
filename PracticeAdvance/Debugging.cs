046735440179e3bc82d8e157099a43e51894887a
gillvineet <gillvineet243@gmail.com>
2024-07-08 13:12:02 +0530

    Just made a change

--- PracticeAdvance/Debugging.cs
+++ PracticeAdvance/Debugging.cs
@@ -20,6 +20,8 @@ namespace PracticeAdvance
             string name = "HI i am vineet gill";
             string[] arr = name.Split(' ');
             Console.WriteLine(c);
+            Console.WriteLine("we are making changes i repo");
+
         }
     }
 }
